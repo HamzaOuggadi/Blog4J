7c1e48150ed9ea25c8632502d454d099a80b25e7
hamzaouggadi <hamza.ouggadi@gmail.com>
2023-03-08 20:33:58 +0100

    Added BlogUserServiceImpl

--- src/main/java/net/hamzaouggadi/blog4j/Blog4jApplication.java
+++ src/main/java/net/hamzaouggadi/blog4j/Blog4jApplication.java
@@ -1,13 +1,36 @@
 package net.hamzaouggadi.blog4j;
 
+import net.hamzaouggadi.blog4j.entities.BlogUser;
+import net.hamzaouggadi.blog4j.repositories.BlogUserRepository;
+import net.hamzaouggadi.blog4j.services.BlogUserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class Blog4jApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(Blog4jApplication.class, args);
+    }
+    @Bean
+    CommandLineRunner start(BlogUserRepository blogUserRepository) {
+        return args -> {
+            Stream.of("Hamza", "Asmae", "Jacky").forEach(usr -> {
+                BlogUser user = new BlogUser();
+                user.setUsername(usr);
+                user.setEmail(usr + "@gmail.com");
+                user.setPassword("1234");
+                user.setCreatedAt(new Date());
+                user.setRemoved(false);
+
+                blogUserRepository.save(user);
+            });
+        };
     }
 
 }
---
+++ src/main/java/net/hamzaouggadi/blog4j/web/BlogUserController.java
@@ -0,0 +1,11 @@
+package net.hamzaouggadi.blog4j.web;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/users")
+public class BlogUserController {
+}
