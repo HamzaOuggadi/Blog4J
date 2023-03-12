9d4fad53afaeda5bd69be6aa63d9fbc484d86eac
hamzaouggadi <hamza.ouggadi@gmail.com>
2023-03-12 12:57:39 +0100

    Working on Comments Service and Controller

--- src/main/java/net/hamzaouggadi/blog4j/services/CommentServiceImpl.java
+++ src/main/java/net/hamzaouggadi/blog4j/services/CommentServiceImpl.java
@@ -5,7 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import net.hamzaouggadi.blog4j.dtos.CommentDTO;
 import net.hamzaouggadi.blog4j.entities.Comment;
 import net.hamzaouggadi.blog4j.mappers.CommentMapper;
-import net.hamzaouggadi.blog4j.repositories.BlogUserRepository;
 import net.hamzaouggadi.blog4j.repositories.CommentRepository;
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Service;
@@ -22,7 +21,6 @@ public class CommentServiceImpl implements CommentService {
     private CommentRepository commentRepository;
     private MessageSource messageSource;
     private CommentMapper commentMapper;
-    private BlogUserRepository blogUserRepository;
 
     @Override
     public List<CommentDTO> listComments() {
@@ -42,27 +40,32 @@ public class CommentServiceImpl implements CommentService {
     @Override
     public List<CommentDTO> getCommentsByUser(Long userId) {
         List<Comment> comments = commentRepository.findByWriterId(userId);
-        log.info("Number of Likes for Comments(1) : " + comments.get(1).getLikes());
         List<CommentDTO> commentDTOS = new ArrayList<>();
         comments.stream().forEach(comment -> {
             commentDTOS.add(commentMapper.commentToCommentDTO(comment));
         });
-        log.info("Number of Likes for CommentDTOs(1) : " + commentDTOS.get(1).getLikes());
         return commentDTOS;
     }
 
     @Override
     public List<CommentDTO> getCommentsByArticle(Long articleId) {
-        return null;
+        List<Comment> comments = commentRepository.findByArticleId(articleId);
+        List<CommentDTO> commentDTOS = new ArrayList<>();
+        comments.stream().forEach(comment -> {
+            commentDTOS.add(commentMapper.commentToCommentDTO(comment));
+        });
+        return commentDTOS;
     }
 
     @Override
     public void addComment(CommentDTO commentDTO) {
-
+        commentRepository.save(commentMapper.commentDTOToComment(commentDTO));
     }
 
     @Override
     public void deleteComment(Long commentId) {
-
+        Comment comment = commentRepository.findById(commentId).orElseThrow();
+        comment.setRemoved(true);
+        commentRepository.save(comment);
     }
 }
